a04b6ecf10c19d05ae87f118df7a205ebe922e56
Pen-123 <confederatepen@gmail.com>
2025-07-30 09:40:32 +0400

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ current_llm = default_llm
 allowed_llms = {
     "llama3-70b": "llama-3.3-70b-versatile",
     "llama3-8b": "llama-3.1-8b-instant",
-    "gemma": "gemma2-9b-it"
+    "gemma2b": "gemma2-9b-it"
 }
 
 def reset_defaults():
